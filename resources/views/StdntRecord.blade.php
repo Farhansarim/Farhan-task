726a7e70a27e4a8f279d5840e2126b05a27a5dc3
farhan <farhansarim92@gmail.com>
2019-03-05 10:59:17 +0500

    blade updated

--- resources/views/StdntRecord.blade.php
+++ resources/views/StdntRecord.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Student Record</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Student Record</title>
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <title>Student Record</title>
+  
 </head>
 <body>
 
-<h1>View Student Record</h1>
+<h1 align="center">View Student Record</h1>
 <table class="table table-striped">
     <tr>
 
